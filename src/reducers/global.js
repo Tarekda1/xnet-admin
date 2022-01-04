68cadc308720591ae524dcab77294eb0e6a4896f
unknown <tare2.da@gmail.com>
2022-01-04 11:38:52 +0200

    all

--- src/reducers/global.js
+++ src/reducers/global.js
@@ -7,7 +7,9 @@ const initialState = {
   showSearching: false,
   subscribers: [],
   appliedFilters: [],
+  status: "all",
   showMenu: false,
+  loaded: false
 };
 
 function addFilterIfNotExists(filter, appliedFilters) {
@@ -36,41 +38,22 @@ export default (state = initialState, action) => {
     case types.CHANGE_LANGUAGE:
       return { ...state, language: payload };
     case types.LOAD_SUBSCRIBERS:
-      return { ...state, subscribers: payload, filteredSubscribers: payload.slice(0), };
+      return { ...state, subscribers: payload, filteredSubscribers: payload.slice(0), loaded: true };
     case types.ADD_SUBSCRIBER:
       return { ...state, subscribers: [...state.subscribers, payload] };
+    case types.CHANGE_FILTER_STATUS:
+      return { ...state, status: payload };
     case types.UPDATE_SUBSCRIBER:
       console.log(payload);
       let myState = Object.assign({}, state);
       let index = myState.filteredSubscribers.findIndex(s => s.username == payload.username);
       console.log(index);
       myState.filteredSubscribers[index] = payload;
+      if (status == "not_paid") {
+        filteredValues = filteredValues.filter(s => s.subscribtionpaid == false);
+        newState.filteredSubscribers = filteredValues;
+      }
       return myState;
-    // case types.FILTER:
-    //   let xState = Object.assign({}, state);
-    //appliedFilters = addFilterIfNotExists(types.FILTER_PAID, appliedFilters);
-    //let appliedFilters = state.appliedFilters;
-    //if (appliedFilters.includes(types.FILTER_SUBSCRIBERS)) {
-    // if (filter == "all") {
-    //   let filteredValues = xState.subscribers;
-    //   xState.filteredSubscribers = filteredValues;
-    // } else {
-    //   let filter = payload.value == "paid";
-    //   let filteredValues = xState.subscribers.filter(s => s.subscribtionpaid == filter);
-    //   xState.filteredSubscribers = filteredValues;
-    // }
-    //}
-    // else {
-    //   if (filter == "all") {
-    //     let filteredValues = xState.subscribers;
-    //     xState.filteredSubscribers = filteredValues;
-    //   } else {
-    //     let filter = payload.value == "paid";
-    //     let filteredValues = xState.subscribers.filter(s => s.subscribtionpaid == filter);
-    //     xState.filteredSubscribers = filteredValues;
-    //   }
-    // }
-    // return xState;
     case types.FILTER_SUBSCRIBERS:
       console.log("hello");
       let newState = Object.assign({}, state);
